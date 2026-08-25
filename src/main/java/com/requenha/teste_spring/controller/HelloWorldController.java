280bf7d17fa5afdd59e15579508b49b40a29af58
Giovanni <142685364+Rivanns@users.noreply.github.com>
2026-08-24 21:31:03 -0300

    Atualiza Hello
    Adiciona nome

--- src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
+++ src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
@@ -1,14 +1,15 @@
 package com.requenha.teste_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
-	@GetMapping("/hello")
-    public String helloWorld() {
-    	return "Hello Spring :)";
+	@GetMapping("/hello/{nome}")
+    public String helloWorld(@PathVariable String nome) {
+    	return "Hello, " + nome;
     }
 	
 	@GetMapping("/goodbye")
