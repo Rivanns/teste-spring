1c81adf20525d9178bcc23f4386f9240ceddc12b
Giovanni <142685364+Rivanns@users.noreply.github.com>
2026-08-31 20:59:28 -0300

    atualiza controller

--- src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
+++ src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.requenha.teste_spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ import com.requenha.teste_spring.dto.HelloWorldResposta;
 @RestController
 public class HelloWorldController {
 	
-	@GetMapping("/hello")
+	@PostMapping("/hello")
     public HelloWorldResposta helloWorld(@RequestBody HelloWorldRequisicao requisicao) {
 		return new HelloWorldResposta("Hello, these are your informations:", requisicao.getNome(), requisicao.getIdade());
     }
