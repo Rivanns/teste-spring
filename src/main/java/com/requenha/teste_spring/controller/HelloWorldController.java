39d5368305f8c3df101721afb202623dafda92b4
Giovanni <142685364+Rivanns@users.noreply.github.com>
2026-08-31 18:58:53 -0300

    atualiza controller

--- src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
+++ src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
@@ -3,13 +3,14 @@ package com.requenha.teste_spring.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.requenha.teste_spring.dto.HelloWorldResposta;
 
 @RestController
 public class HelloWorldController {
 	
 	@GetMapping("/hello/{nome}/{idade}")
-    public String helloWorld(@PathVariable String nome, @PathVariable int idade) {
-    	return "Hello, " + nome + " you are " + idade + " years old.";
+    public HelloWorldResposta helloWorld(@PathVariable String nome, @PathVariable int idade) {
+    	return new HelloWorldResposta("Hello, " + nome + " you are " + idade + " years old.");
     }
 	
 	@GetMapping("/goodbye")
--- src/main/java/com/requenha/teste_spring/dto/HelloWorldResposta.java
+++ src/main/java/com/requenha/teste_spring/dto/HelloWorldResposta.java
@@ -1,5 +1,18 @@
 package com.requenha.teste_spring.dto;
 
 public class HelloWorldResposta {
-
+	
+	private String mensagem;
+	
+	public HelloWorldResposta(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	public String getMensagem() {
+		return mensagem;
+	}
+	
+	public void setMensagem (String mensagem) {
+		this.mensagem = mensagem;
+	}
 }
