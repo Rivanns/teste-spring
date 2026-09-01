a137b6ba576485a31c3c1c9d396c4aaa2d431c50
Giovanni <142685364+Rivanns@users.noreply.github.com>
2026-08-31 21:20:43 -0300

    atualiza e implementa service

--- src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
+++ src/main/java/com/requenha/teste_spring/controller/HelloWorldController.java
@@ -1,20 +1,25 @@
 package com.requenha.teste_spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.requenha.teste_spring.dto.HelloWorldRequisicao;
 import com.requenha.teste_spring.dto.HelloWorldResposta;
+import com.requenha.teste_spring.service.HelloWorldService;
 
 @RestController
 public class HelloWorldController {
 	
+	@Autowired
+	HelloWorldService servico;
+	
 	@PostMapping("/hello")
     public HelloWorldResposta helloWorld(@RequestBody HelloWorldRequisicao requisicao) {
-		return new HelloWorldResposta("Hello, these are your informations:", requisicao.getNome(), requisicao.getIdade());
-    }
+		//return new HelloWorldResposta("Hello, these are your informations:", requisicao.getNome(), requisicao.getIdade());
+		return servico.helloWorld(requisicao);
+	}
 	
 	@GetMapping("/goodbye")
 	public String goodbyeWorld() {
--- src/main/java/com/requenha/teste_spring/service/HelloWorldService.java
+++ src/main/java/com/requenha/teste_spring/service/HelloWorldService.java
@@ -1,5 +1,14 @@
 package com.requenha.teste_spring.service;
 
-public class HelloWorldService {
+import org.springframework.stereotype.Service;
 
+import com.requenha.teste_spring.dto.HelloWorldRequisicao;
+import com.requenha.teste_spring.dto.HelloWorldResposta;
+
+@Service
+public class HelloWorldService {
+	
+	public HelloWorldResposta helloWorld(HelloWorldRequisicao requisicao) {
+		return new HelloWorldResposta("Hello, these are your informations:", requisicao.getNome(), requisicao.getIdade());
+	}	
 }
